117ed8ef452f1a3ed74c6c5b6000b2149e71d2b7
rsca7213 <rsca4321@gmail.com>
2020-03-30 20:37:29 -0400

    changed lang of error messages to spanish

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Su contraseña ha sido reiniciada!',
+    'sent' => 'Le enviamos un correo con los pasos a tomar!',
+    'throttled' => 'Por favor espera unos segundos antes de intentarlo de nuevo.',
+    'token' => 'Token invalido.',
+    'user' => "No hay ningun usuario con este correo electrónico.",
 
 ];
